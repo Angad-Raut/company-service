69998450c0fba32c52b44980cf3325519c5ef73c
Angad-Raut <85058287+Angad-Raut@users.noreply.github.com>
2024-09-08 16:45:25 +0530

    changes commited

--- src/main/java/com/projectx/company_service/CompanyServiceApplication.java
+++ src/main/java/com/projectx/company_service/CompanyServiceApplication.java
@@ -2,14 +2,20 @@ package com.projectx.company_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class CompanyServiceApplication {
+public class CompanyServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyServiceApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CompanyServiceApplication.class);
+	}
 }
